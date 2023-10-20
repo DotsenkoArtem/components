9553cc58fedec0f21452dec9cb51abd7550a0e5f
DotsenkoArtem <artemdoc1@rambler.ru>
2023-10-20 16:53:23 +0500

    Added scroll-bar-indicator for testing

---
+++ test/scrollbar-indicator/public/js/app.dev.js
@@ -0,0 +1,14 @@
+"use strict";
+
+var scrollBarIndicator = document.querySelector('.scroll-bar-indicator');
+var scrollBarIndicatorStrip = scrollBarIndicator.querySelector('.scroll-bar-indicator__strip');
+var documentHeight = document.documentElement.offsetHeight;
+var windowHeight = document.documentElement.clientHeight;
+
+function setScrollBarIndicator() {
+  var scrollProgress = window.scrollY;
+  scrollBarIndicatorStrip.style.width = "".concat(Math.floor(scrollProgress / (documentHeight - windowHeight) * 10000) / 100, "%");
+}
+
+setScrollBarIndicator();
+window.addEventListener('scroll', setScrollBarIndicator); // ДОДБАВИТЬ ИЗМЕНЕНИЕ ПРИ РЕСАЙЗЕ
---
+++ test/scrollbar-indicator/src/js/app.js
@@ -0,0 +1,21 @@
+"use strict"
+
+const scrollBarIndicator = document.querySelector('.scroll-bar-indicator')
+const scrollBarIndicatorStrip = scrollBarIndicator.querySelector('.scroll-bar-indicator__strip')
+const documentHeight = document.documentElement.offsetHeight
+const windowHeight = document.documentElement.clientHeight
+
+function setScrollBarIndicator() {
+  let scrollProgress = window.scrollY
+  scrollBarIndicatorStrip.style.width = `${Math.floor(scrollProgress/(documentHeight - windowHeight) * 10000) / 100}%`
+}
+
+setScrollBarIndicator()
+
+window.addEventListener('scroll', setScrollBarIndicator)
+
+
+
+// ДОДБАВИТЬ ИЗМЕНЕНИЕ ПРИ РЕСАЙЗЕ
+
+
